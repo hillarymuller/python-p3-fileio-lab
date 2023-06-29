8cc726505c396bcb0789d5df7f67890c65232ce3
Hillary Muller <hillaryaward@gmail.com>
2023-06-29 18:00:56 -0500

    open and add to file

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as my_file:
+        my_file.write(f'{file_content}')
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a', encoding="utf-8") as my_file:
+        my_file.write(f'{append_content}')
 
 def read_file(file_name):
-    pass
+    my_file = open(f'{file_name}.txt', encoding="utf-8")
+    return my_file.read()
